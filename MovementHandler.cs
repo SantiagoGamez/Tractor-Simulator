bfe229f1695d95214534fd16b8567e203bfcdeaf
SantiagoGamez <105666741+SantiagoGamez@users.noreply.github.com>
2023-11-13 10:49:27 -0600

    Add files via upload
    JSON text format:
    {     "X_COORDINATE": 0.0,     "Y_COORDINATE": 0.0,     "Z_COORDINATE": 0.0 }

---
+++ MovementHandler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System;
+
+[Serializable]
+public class PositionData
+{
+    public float X_COORDINATE;
+    public float Y_COORDINATE;
+    public float Z_COORDINATE;
+}
+
+public class MovementHandler : MonoBehaviour
+{
+    public string jsonText;
+    private string lastJsonText;
+    private Vector3 targetPosition;
+
+    void Start()
+    {
+        lastJsonText = jsonText;
+        UpdateTargetPosition();
+    }
+
+    void Update()
+    {
+        if (jsonText != lastJsonText)
+        {
+            lastJsonText = jsonText;
+            UpdateTargetPosition();
+        }
+
+        // Move the object smoothly to the target position
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime);
+    }
+
+    void UpdateTargetPosition()
+    {
+        PositionData data = JsonUtility.FromJson<PositionData>(jsonText);
+        targetPosition = new Vector3(data.X_COORDINATE, data.Y_COORDINATE, data.Z_COORDINATE);
+    }
+}
